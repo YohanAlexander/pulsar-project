486acb278ad1086370a3eefc869436d30f0c9a9b
Yohan Alexander <yohanfranca@gmail.com>
2019-03-02 00:11:27 -0300

    Add files via upload

---
+++ sensor.c
@@ -0,0 +1,40 @@
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>      
+
+int ADCsingleREAD(uint8_t adctouse){
+
+    int ADCval;
+
+    ADMUX = adctouse;        // Usa #1 ADC
+    ADMUX |= (1 << REFS0);   // Usa AVcc como referência
+    ADMUX &= ~(1 << ADLAR);  // Limpa para a resolução de 10 bits
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128 prescala para 8Mhz
+    ADCSRA |= (1 << ADEN);    // Ativa o ADC
+    ADCSRA |= (1 << ADSC);    // Começa a conversão do ADC
+    
+    while(ADCSRA & (1 << ADSC)); // Espera o ADC terminar
+
+    ADCval = ADCL;
+    ADCval = (ADCH << 8) + ADCval; // Concatena os bits mais e menos significativos
+
+    return ADCval;
+}
+
+int Sinal;
+float Lux;
+
+int main (void){
+
+    Serial.begin(9600); // Velocidade padrão da comunição serial
+
+    while (1){
+
+    Sinal = ADCsingleREAD(0); // Leitura do pino analógico
+    Lux = Sinal * 0.9765625; // Conversão para valor de luminosidade
+    Serial.println(Lux); // Impressão da luminosidade no serial
+    _delay_ms(5);
+    
+    }
+}
---
+++ tabela.py
@@ -0,0 +1,40 @@
+# Importa as bibliotecas
+import serial, time, datetime, csv
+
+# Valor lógico que representa a conexão do arduino
+connectado = False
+
+# Estabelece a conexão a porta serial do arduino
+portas = ['/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3','COM1', 
+'COM2', 'COM3', 'COM4','COM5', 'COM6','COM7', 'COM8', 'COM9','COM10','COM11']
+
+for dispositivo in portas:
+    try:
+        print("\nConectando... %s" %(dispositivo))
+        arduino = serial.Serial(port = dispositivo, baudrate = 9600, timeout = 1)
+        break
+    except:
+        print("Conexão falhou com: %s \n" %(dispositivo))
+
+# Laço até estabelecer a conexão com o arduino
+while not connectado:
+    porta = arduino.read()
+    arduino.flushInput()
+    connectado = True
+    print("Conectado a porta: %s \n" %(arduino.portstr))
+
+# Decodifica a saída serial do arduino e salva no arquivo csv
+while True:
+    try:   
+        with open('tabela.csv', mode = 'w', newline = '') as tabela:
+            while True:
+                linha = arduino.readline().decode('utf-8').strip('\r\n')
+                tempo = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f')
+                arquivo = csv.writer(tabela, delimiter = ",")
+                arquivo.writerow([tempo, linha])
+    except:
+        print("Conexão Interrompida\n")
+        break
+
+tabela.close()
+arduino.close()
