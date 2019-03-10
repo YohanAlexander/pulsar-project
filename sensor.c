ad477b6d74db4164b913908cd982e13b4cf934bf
YohanAlexander <yohanfranca@gmail.com>
2019-03-10 17:10:08 -0300

    Update sensor.c

--- sensor.c
+++ sensor.c
@@ -1,7 +1,6 @@
-
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdint.h>      
+#include <stdint.h>
 
 int ADCsingleREAD(uint8_t adctouse){
 
@@ -10,8 +9,9 @@ int ADCsingleREAD(uint8_t adctouse){
     ADMUX = adctouse;        // Usa #1 ADC
     ADMUX |= (1 << REFS0);   // Usa AVcc como referência
     ADMUX &= ~(1 << ADLAR);  // Limpa para a resolução de 10 bits
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128 prescala para 8Mhz
-    ADCSRA |= (1 << ADEN);    // Ativa o ADC
+    
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128 prescaler para 8Mhz
+    ADCSRA |= (1 << ADEN);    // Habilita o ADC
     ADCSRA |= (1 << ADSC);    // Começa a conversão do ADC
     
     while(ADCSRA & (1 << ADSC)); // Espera o ADC terminar
@@ -34,7 +34,7 @@ int main (void){
     Sinal = ADCsingleREAD(0); // Leitura do pino analógico
     Lux = Sinal * 0.9765625; // Conversão para valor de luminosidade
     Serial.println(Lux); // Impressão da luminosidade no serial
-    _delay_ms(5);
+    _delay_ms(5); // Aguarda 5 milisegundos
     
     }
 }
