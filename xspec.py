9993b88fa6272ba2590ee0f3f934bad285d00ead
Yohan Alexander <yohanfranca@gmail.com>
2019-07-29 23:56:38 -0300

    cli

--- xspec.py
+++ xspec.py
@@ -4,6 +4,9 @@
 # Generic/Built-in
 import sys, subprocess
 
+# Other libs
+import click
+
 __author__ = 'Yohan Alexander'
 __copyright__ = 'Copyright 2019, Xspec Script' 
 __credits__ = ['Yohan Alexander']
@@ -14,9 +17,10 @@ __email__ = 'yohanfranca@gmail.com'
 __status__ = 'Dev'
 
 class Parameters():
-        def __init__(self, folder, expo):
+        def __init__(self, expo, folder=""):
             self.program = ["xspec"]
             self.dir = "cd %s\n" %folder
+            self.cpd = "cpd /xs\n"
             self.model = "model apec*phabs\n"
             self.kT = "12\n"
             self.abundancy = "0.3\n"
@@ -39,20 +43,32 @@ class Parameters():
             self.parallel = "parallel error 3\n"
             self.err = "err 1 4\n"
             self.setplot = "setplot energy\n"
-            self.iplot = "iplot data\n"
-            self.quit = "quit\n"
-            self.cpd = "cpd %s.ps/cps\n" %expo
-            self.data = "plot data\n"
-            self.ldata = "plot ldata\n"
-            self.none = "cpd none\n"
+            self.ldata = "plot ldata chi\n"
+            self.iplot = "iplot\n"
+            self.timeoff = "time off\n"
+            self.labeltop = "label top Simulated Spectrum with exposition time of %s seconds\n" %expo
+            self.labelfile = "label file Gamma Cassiopeiae\n"
+            self.labely = "label y counts s\\u-1\\d keV\\u-1\\d\n"
+            self.csize = "csize 0.8\n"
+            self.plot = "plot\n"
+            self.hard = "hard %s.ps/cps\n" %expo
+            self.logy = "log y\n"
+            self.rescale = "rescale y 0.1 100\n"
+            self.plot2 = "plot\n"
+            self.hard2 = "hard l%s.ps/cps\n" %expo
+            self.quit = "quit.ps/cps\n"
             self.exit = "exit\n"
 
-def main(args):
 
-    folder = args[1]
-    expo = args[2]
+@click.command()
+@click.argument('exposition')
+@click.option('--folder', '-f', help='full or relative path to your pn file')
+def main(exposition, folder):
+    """
+    Python script to automate the xspec spectral fitting
+    """
 
-    i = Parameters(folder, expo)
+    i = Parameters(exposition, folder)
 
     xspec = subprocess.Popen(i.program, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE, universal_newlines=True, bufsize=0)
@@ -83,12 +99,21 @@ def main(args):
     xspec.stdin.write(i.parallel)
     xspec.stdin.write(i.err)
     xspec.stdin.write(i.setplot)
-    xspec.stdin.write(i.iplot)
-    xspec.stdin.write(i.quit)
-    xspec.stdin.write(i.cpd)
-    xspec.stdin.write(i.data)
     xspec.stdin.write(i.ldata)
-    xspec.stdin.write(i.none)
+    xspec.stdin.write(i.iplot)
+    xspec.stdin.write(i.timeoff)
+    xspec.stdin.write(i.labeltop)
+    xspec.stdin.write(i.labelfile)
+    xspec.stdin.write(i.labely)
+    xspec.stdin.write(i.csize)
+    xspec.stdin.write(i.plot)
+    xspec.stdin.write(i.hard)
+    xspec.stdin.write(i.logy)
+    xspec.stdin.write(i.rescale)
+    xspec.stdin.write(i.plot2)
+    xspec.stdin.write(i.hard2)
+    xspec.stdin.write(i.quit)
+
     xspec.stdin.write(i.exit)
     
     xspec.stdin.close()
@@ -97,4 +122,4 @@ def main(args):
         print(line)
         
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
